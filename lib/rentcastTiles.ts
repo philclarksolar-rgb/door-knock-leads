11c61cea367bdee2fe3f2fbf4ed0c0cab1325871
philclarksolar-rgb <philclarksolar@gmail.com>
2026-03-09 02:05:24 -0700

    Create rentcastTiles.ts

---
+++ lib/rentcastTiles.ts
@@ -0,0 +1,12 @@
+export const TILE_SIZE = 0.1;
+
+export function tileCoord(value: number) {
+  return Math.floor(value / TILE_SIZE) * TILE_SIZE;
+}
+
+export function getTile(lat: number, lon: number) {
+  return {
+    tileLat: tileCoord(lat),
+    tileLon: tileCoord(lon)
+  };
+}
