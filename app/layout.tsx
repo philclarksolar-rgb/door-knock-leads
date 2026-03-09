05e39efbe03e18874d96a9bc8faf33fc3e959533
philclarksolar-rgb <philclarksolar@gmail.com>
2026-03-08 22:16:36 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="bg-slate-50 text-slate-900">
+        {children}
+      </body>
     </html>
   );
 }
