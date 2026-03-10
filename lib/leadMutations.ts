71b60c0b4c186b3dedb9a0e120089d21c2726961
philclarksolar-rgb <philclarksolar@gmail.com>
2026-03-09 23:18:14 -0700

    Update leadMutations.ts

--- lib/leadMutations.ts
+++ lib/leadMutations.ts
@@ -1,5 +1,4 @@
 export { createLeadRecord } from "./leadMutations/createLead";
-export { updateLeadRecord } from "./leadMutations/updateLead";
-export { deleteLeadRecord } from "./leadMutations/updateLead";
+export { updateLeadRecord, deleteLeadRecord } from "./leadMutations/updateLead";
 export { addContactLogRecord } from "./leadMutations/contactLog";
 export { addNoteRecord } from "./leadMutations/notes";
